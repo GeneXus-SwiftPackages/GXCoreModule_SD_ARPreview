909157e64c183120adb47945387cc72b59b71e5b
genexusbot <builder@genexus.com>
2023-03-09 12:48:32 -0300

    Package version: 1.0.0-beta+20230309154144

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
 		.target(name: "GXCoreModule_SD_ARPreviewWrapper",
 				dependencies: [
 					"GXCoreModule_SD_ARPreview",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS, .watchOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_ARPreview",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_ARPreview-1.0.0-beta+20230309150554.xcframework.zip",
-			checksum: "3a92b59209dc0dd60fa6428f29a9869aae2d2acf1ec798e7716f299a967a8569"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_ARPreview-1.0.0-beta+20230309154144.xcframework.zip",
+			checksum: "6e7760ef3bb7d8b6bb36f22556d38cd0f33e981809758c54e3b38172ef9ba8d6"
 		)
 	]
 )
