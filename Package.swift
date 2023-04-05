9f58fada6871225daa787f174f42401915f9a933
Build Er <builder@macmini6.local>
2023-04-05 10:40:53 -0300

    Package version: 1.0.0-beta+20230405133406

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_ARPreview",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_ARPreview-1.0.0-beta+20230330152753.xcframework.zip",
-			checksum: "3bb84684823d2a8674866a6b802d067cd0fa88de0c2e9b4819582f93773e8acc"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_ARPreview-1.0.0-beta+20230405133406.xcframework.zip",
+			checksum: "092c30726f378338b52e7c1ab6d10d002f411bbaafc82fb01621b3731f4e7356"
 		)
 	]
 )
