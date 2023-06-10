1b972c8ccdb0529e79f4785562bc4039ea38349b
Build Er <builder@macmini6.local>
2023-06-10 02:31:34 -0300

    Package version: 1.0.0-beta+20230610052334

--- Package.swift
+++ Package.swift
@@ -10,19 +10,19 @@ let package = Package(
 			targets: ["GXCoreModule_SD_ARPreviewWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", revision: "02fd74284a30115df5e5c22f8b14f44194752676")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", revision: "8d33dc7510f9908c8b1814a960f6afdd974e9c43")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_ARPreviewWrapper",
 				dependencies: [
 					"GXCoreModule_SD_ARPreview",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .watchOS, .iOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.watchOS, .tvOS, .iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_ARPreview",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_ARPreview-1.0.0-beta+20230609175617.xcframework.zip",
-			checksum: "6f56f4cfc38a46ad6fc06974c7c132907a18b427a0dab6cd580824b4cbb3b4a0"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_ARPreview-1.0.0-beta+20230610052334.xcframework.zip",
+			checksum: "c9a9ca7db9d05b713db38ecd730b48f5d837d0679b345a97d8a4ef4aeff7bc79"
 		)
 	]
 )
