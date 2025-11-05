15314298b609c5b853ab834c109996aa0741f5ac
The iOS Team @ GeneXus <builder@genexus.com>
2025-11-05 11:39:44 -0500

    Package version: 4.1.0-beta.5

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_SD_ARPreviewWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.1.0-beta.4")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.1.0-beta.5")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_ARPreviewWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_ARPreview",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_ARPreview-4.1.0-beta.4.xcframework.zip",
-			checksum: "50d22a657387ded52009b741b5d059988b7b5463e0a1f0aa984f4a96c78037d6"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_ARPreview-4.1.0-beta.5.xcframework.zip",
+			checksum: "c40d2ca08d811102e31c5f8ccbaa2d3dacdd465b5fb01b52028933f552ed77e5"
 		)
 	]
 )
